4ac2b58da747a74389fb2f2e1f417f5ee7e8bb44
HegarSol <93796924+HegarSol@users.noreply.github.com>
2024-06-05 12:20:46 -0500

    agregando camops a la consulta del reporte api

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:85/getcfdi/';
+$config['base_url'] = 'http://localhost:85/git_hub_repo/avanza_buzon_github/';
 
 /*
 |--------------------------------------------------------------------------
